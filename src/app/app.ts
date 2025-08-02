cb7c832c60ce4a365b388063cbdd2ad12abff776
brunoolmrezende <brunoolmrezende303@gmail.com>
2025-08-02 17:46:15 -0300

    feat: Finish certificate content

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,10 +7,11 @@ import { ItemCertificate } from './_components/item-certificate/item-certificate
 import { BaseUi } from "./_components/base-ui/base-ui";
 import { Certificates } from './pages/certificates/certificates';
 import { CertificateForm } from "./pages/certificate-form/certificate-form";
+import { Certificate } from './pages/certificate/certificate';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificate, BaseUi, Certificates, CertificateForm],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificate, BaseUi, Certificates, CertificateForm, Certificate],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

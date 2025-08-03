be6b134d01681a980347b90ea56e5353decbdf32
Bruno Rezende <brunoolmrezende303@gmail.com>
2025-08-03 14:04:55 -0300

    feat: configure routes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./_components/navbar/navbar";
-import { PrimaryButton } from './_components/primary-button/primary-button';
-import { SecondaryButton } from './_components/secondary-button/secondary-button';
-import { ItemCertificate } from './_components/item-certificate/item-certificate';
 import { BaseUi } from "./_components/base-ui/base-ui";
-import { Certificates } from './pages/certificates/certificates';
-import { CertificateForm } from "./pages/certificate-form/certificate-form";
-import { Certificate } from './pages/certificate/certificate';
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificate, BaseUi, Certificates, CertificateForm, Certificate],
+  imports: [RouterOutlet, Navbar, BaseUi],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

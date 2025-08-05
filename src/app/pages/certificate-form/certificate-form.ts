e9824ea0b4727e5a2a0f561658893e882aa42cd1
brunoolmrezende <brunoolmrezende303@gmail.com>
2025-08-04 22:58:52 -0300

    chore: update Angular dependencies to version 20.1.4
    feat: add buttonText input to secondary-button component for dynamic button text
    fix: update item-certificate component to use buttonText input
    
    style: adjust navbar styles for active links and improve button alignment
    
    refactor: enhance primary-button component to accept buttonText and isButtonDisabled inputs
    fix: update certificate-form to use ngForm and validate inputs correctly
    
    feat: implement activity list management in certificate-form component
    fix: add redirect functionality in certificates component for generating new certificates

--- src/app/pages/certificate-form/certificate-form.ts
+++ src/app/pages/certificate-form/certificate-form.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
 import { PrimaryButton } from "../../_components/primary-button/primary-button";
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-certificate-form',
-  imports: [SecondaryButton, PrimaryButton],
+  imports: [SecondaryButton, PrimaryButton, FormsModule],
   templateUrl: './certificate-form.html',
   styleUrl: './certificate-form.css'
 })
 export class CertificateForm {
+  name: string = '';
+  activity: string = '';
+  activities: string[] = [''];
 
+  isInvalidField(control: NgModel) {
+    return control.invalid && control.touched;
+  }
+
+  isValidForm() {
+    return this.name.length > 0 && this.activities.length > 0;
+  }
 }
--- src/app/pages/certificates/certificates.ts
+++ src/app/pages/certificates/certificates.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { ItemCertificate } from "../../_components/item-certificate/item-certificate";
+import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-certificates',
-  imports: [ItemCertificate],
+  imports: [ItemCertificate, SecondaryButton],
   templateUrl: './certificates.html',
   styleUrl: './certificates.css'
 })
 export class Certificates {
+  constructor(private router: Router) {}
 
+  redirectToGenerateCertificate() {
+    this.router.navigate(['/certificates/new']);
+  }
 }
